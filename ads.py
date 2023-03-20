9fc3c34548ca42935a42dc956e166ffee0d57d81
Mohit <mohit810@gmail.com>
2023-03-20 19:17:35 +0530

    first commit

---
+++ ads.py
@@ -0,0 +1,31 @@
+css_string = """
+             <style>
+
+            .sidebar {max-width:50%; float:left}
+            .side-block img {width:100%}
+            .side-block {margin-bottom:15px}
+            .side-block a.button {margin-bottom:30px; background: #006CFF; color: #fff;font-weight: 500;font-size: 16px;line-height: 20px;padding: 15px;display: inline-block;max-width: 300px;border-radius: 5px; text-decoration:none;   }
+            .side-block a.button:hover {background:#000}
+
+            </style> 
+
+            <div class="sidebar">
+
+            <div class="side-block">
+            <a target="_blank" href="https://google.com" rel="noopener">
+            <img src="https://www.wordstream.com/wp-content/uploads/2021/07/persuasive-ads-coca-cola.jpg" alt="ad1">
+            </div>
+            <div class="side-block">
+            <a href="https://google.com" class="button ">dummy button</a>
+            <div class="side-block">
+            </div>
+            <a target="_blank" href="https://google.com" rel="noopener">
+            <img src="https://www.wordstream.com/wp-content/uploads/2021/07/persuasive-ads-coca-cola.jpg" alt="ad2">
+            </div>
+            <div class="side-block">
+            <a target="_blank" href="https://google.com" rel="noopener">
+            <img src="https://www.wordstream.com/wp-content/uploads/2021/07/persuasive-ads-coca-cola.jpg" alt="ad3"></a>
+            </div>
+            </div> 
+
+            """
